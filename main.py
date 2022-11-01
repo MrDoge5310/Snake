41a3e407f3d9bd4701ff92c6d38689b37b2f3db7
MrDoge5310 <daniiltrotsenko2002@gmail.com>
2022-11-01 12:29:10 +0200

    1 version, snake don't move

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
 import pygame
+import random
 
 
 class Block:
     """block"""
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -15,50 +17,60 @@ class Block:
 
     def DrawBlock(self):
         """drawing a part of a snake"""
+        self.ClearPos()
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.width)
         pygame.draw.rect(screen, 'blue', self.rect, width=0)
 
 
 class Snake:
     """snake"""
+
     def __init__(self):
         """cords and length"""
-        self.size = 2
         self.direction = "top"
-        self.head = Block(width / 2, high / 2)
-        self.body = [self.head, Block(self.head.x, self.head.y + 20)]
+        self.head = Block(600, 400)
+        self.body = [self.head, Block(self.head.x, self.head.y + 20), Block(self.head.x, self.head.y + 40)]
+        self.size = len(self.body)
 
     def drawSnake(self):
         """drawing snake"""
         for i in range(self.size):
             self.body[i].DrawBlock()
 
-    def move(self, direction):
-        self.direction = direction
-        for i in range(self.size):
-            self.body[i].ClearPos()
+    def move(self):
+        tempX = self.head.x
+        tempY = self.head.y
 
-            if self.direction == "left":
-                self.body[i].x -= self.body[i].width
-            if self.direction == "right":
-                self.body[i].x += self.body[i].width
-            if self.direction == "top":
-                self.body[i].y -= self.body[i].width
-            if self.direction == "down":
-                self.body[i].y += self.body[i].width
+        if self.direction == "top":
+            self.head.y -= 20
+        elif self.direction == "down":
+            self.head.y += 20
+        elif self.direction == "left":
+            self.head.x -= 20
+        elif self.direction == "right":
+            self.head.x += 20
+
+        for i in range(self.size):
+            if i > 0:
+                self.body[i].x = tempX
+                self.body[i].y = tempY
+                tempX = self.body[i].x
+                tempY = self.body[i].y
 
     def growUp(self):
-        """growing up the snake"""
+        self.size += 1
+        self.body.append(Block(self.body[self.size - 1].x, self.body[self.size - 1].y))
 
 
 def GetRandomCords():
-    """Getting random cords (x, y)"""
+    x = random.randint(0, 1200)
+    y = random.randint(0, 800)
 
 
 pygame.init()
 clock = pygame.time.Clock()
-GameOver = False
 
-FPS = 2
+FPS = 3
 high = 800
 width = 1200
 screen = pygame.display.set_mode((width, high))
@@ -66,21 +78,26 @@ screen = pygame.display.set_mode((width, high))
 snake_ = Snake()
 snake_.drawSnake()
 
-while not GameOver:
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                snake_.move("left")
+                if snake_.direction != "right":
+                    snake_.direction = "left"
             elif event.key == pygame.K_RIGHT:
-                snake_.move("right")
+                if snake_.direction != "left":
+                    snake_.direction = "right"
             elif event.key == pygame.K_UP:
-                snake_.move("top")
+                if snake_.direction != "down":
+                    snake_.direction = "top"
             elif event.key == pygame.K_DOWN:
-                snake_.move("down")
+                if snake_.direction != "top":
+                    snake_.direction = "down"
 
+    snake_.move()
     snake_.drawSnake()
-    clock.tick(FPS)
     pygame.display.flip()
+    clock.tick(FPS)
